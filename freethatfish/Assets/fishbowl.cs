01804b935785f973e1e445dab5cf72e460cb5428
yerathel27 <pedro.mendes@rtwimaginarylabs.com>
2015-05-10 19:51:32 +0100

    editor_building

---
+++ freethatfish/Assets/fishbowl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishbowl : MonoBehaviour {
+
+	public enum typ{regular,square};
+	public enum lid{none,cork,stun};
+	public enum water{regular,inked};
+	public enum state{regular,crack,leak};
+
+
+	public typ _typ;
+	public lid _lid;
+	public water _water;
+	public state _state;
+	public bool isSticky;
+
+}
